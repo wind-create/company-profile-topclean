f5f70c76a8e6576935d87fb0982be59ac1bb3628
wind_create <nardowilli@gmail.com>
2026-01-20 00:13:25 +0700

    error util solve

---
+++ utils/formatPrice.ts
@@ -0,0 +1,7 @@
+export function formatPrice(value: number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(value);
+}
