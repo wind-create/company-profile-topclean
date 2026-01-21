1ee3b0394d41e55c5a52014d76b3631e9dd760e1
wind_create <nardowilli@gmail.com>
2026-01-22 01:32:08 +0700

    perbaikan pada topclean

--- components/atoms/Logo.tsx
+++ components/atoms/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <Image
-      src="/images/logo.tiff"
+      src="/images/logo.png"
       alt="TopClean"
       width={120}
       height={60}
